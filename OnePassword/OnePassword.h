45be768dc410ba66055901f295370b88af47dceb
Jessica Ip <ipjessica@gmail.com>
2016-11-24 14:20:25 -0500

    Added one password files to a project (for use as a framework)

---
+++ OnePassword/OnePassword.h
@@ -0,0 +1,19 @@
+//
+//  OnePassword.h
+//  OnePassword
+//
+//  Created by Jessica Ip on 2016-11-24.
+//  Copyright © 2016 Shopkey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for OnePassword.
+FOUNDATION_EXPORT double OnePasswordVersionNumber;
+
+//! Project version string for OnePassword.
+FOUNDATION_EXPORT const unsigned char OnePasswordVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OnePassword/PublicHeader.h>
+
+#import <OnePassword/OnePasswordExtension.h>
